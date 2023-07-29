37c7b7c578728b7d0ee6f1d8e2e18da0244df60f
reham238 <139008756+reham238@users.noreply.github.com>
2023-07-29 20:22:50 +0300

    Update mighty_luulia1.ino

--- mighty_luulia1.ino
+++ mighty_luulia1.ino
@@ -1,5 +1,5 @@
 // C++ code
-//
+// delay(250);
 int i;
 int reading1 =0;
 int reading2 =0;
@@ -13,6 +13,7 @@ void setup()
   pinMode(11,OUTPUT);
   pinMode(A5,INPUT);
   pinMode(A4,INPUT);
+  
 }
 
 void loop()
@@ -20,35 +21,26 @@ void loop()
   reading1=digitalRead(A5);
   if(reading1==1)
   {
-    digitalWrite(3,1);
-    digitalWrite(5,1);
+    analogWrite(3,128);
+    analogWrite(11,128);
   }
   else if(reading1==0)
   {
-for(i=255;i>=0;i-=5)
-{
- analogWrite(3,i);
- analogWrite(5,i);
-  delay(250);
+  analogWrite(3,0);
+  analogWrite(11,0);
 }
-    
-  }
- delay(1000);
-   reading2=digitalRead(A4);
+   
+     reading2=digitalRead(A4);
   if(reading2==1)
   {
     digitalWrite(9,1);
-    digitalWrite(11,1);
+    digitalWrite(5,1);
   }
   else if(reading2==0)
   {
-for(i=255;i>=0;i-=5)
-{
- analogWrite(9,i);
- analogWrite(11,i);
-  delay(250);
-}
+
+    digitalWrite(9,0);
+    digitalWrite(5,0);
     
   }
- delay(1000);
 }
